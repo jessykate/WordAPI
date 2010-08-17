326a0a4b3dfeac4d823f9d6eaa4f96a9203620d3
jessykate <jessy.cowansharp@gmail.com>
2010-08-17 10:48:21 -0400

    API urls should end in dot-json and not slash-json

--- wordapi/api/urls.py
+++ wordapi/api/urls.py
@@ -9,15 +9,15 @@ from piston.resource import Resource
 
 urlpatterns = patterns('',
   # tokenize the contents of the url passed in the request                       
-  url(r'^tokenize/url/(?P<emitter_format>.+)$', Resource(UrlTokenHandler)),
+  url(r'^tokenize/url\.(?P<emitter_format>.+)$', Resource(UrlTokenHandler)),
   # tokenize the contents of the content passed in the request
-  url(r'^tokenize/request/(?P<emitter_format>.+)$', Resource(RequestTokenHandler)),
+  url(r'^tokenize/request\.(?P<emitter_format>.+)$', Resource(RequestTokenHandler)),
   # calculate the token frequency of the contents at the url passed in                       
-  url(r'^frequency/url/(?P<emitter_format>.+)$', Resource(UrlFrequencyHandler)),
+  url(r'^frequency/url\.(?P<emitter_format>.+)$', Resource(UrlFrequencyHandler)),
   # calculate the token frequency of the contents passed in                       
-  url(r'^frequency/request/(?P<emitter_format>.+)$', Resource(RequestFrequencyHandler)),
+  url(r'^frequency/request\.(?P<emitter_format>.+)$', Resource(RequestFrequencyHandler)),
   # build a tag cloud from the contents passed in 
-  url(r'^tagcloud/(?P<emitter_format>.+)$', Resource(TagCloudHandler)),
+  url(r'^tagcloud\.(?P<emitter_format>.+)$', Resource(TagCloudHandler)),
 
 
 )
