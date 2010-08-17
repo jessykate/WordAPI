2a3302d2620ff5d3a9195e83be90237753737a53
jessykate <jessy.cowansharp@gmail.com>
2010-08-17 10:53:33 -0400

    added CSRF handling

--- wordfrontend/frontend/views.py
+++ wordfrontend/frontend/views.py
@@ -1,6 +1,8 @@
 # Create your views here.
 from django.conf import settings
 from django.shortcuts import render_to_response
+from django.template import RequestContext
+from django.core.context_processors import csrf
 from frontend.forms import TagCloudForm
 import urllib, httplib2
 try:
@@ -8,13 +10,17 @@ try:
 except:
     import simplejson as json
 
+def render_to_template(request, template, kwargs):
+    kwargs.update(csrf(request))
+    return render_to_response(template, kwargs)
+
 def Index(request):
     return render_to_response('frontend/index.html')
 
 def tagcloud(request):
     if request.method == 'GET':
         form = TagCloudForm()
-        return render_to_response('frontend/tagcloud.html', {'domain': settings.ROOT_URL,'tagcloud_form' : form })
+        return render_to_template(request, 'frontend/tagcloud.html', {'domain': settings.ROOT_URL,'tagcloud_form' : form })
 
     else:
         form = TagCloudForm(request.POST)
@@ -22,18 +28,14 @@ def tagcloud(request):
         # probably validate tokenizer regular expressions etc. 
         if form.is_valid(): 
             data = form.cleaned_data
-            print data
             args = {}
             for field, value in data.iteritems():
                 if value:
                     args[field] = value
-
-            url = settings.ROOT_URL + "/api/1.0/tagcloud/json"
+            
+            url = settings.ROOT_URL + "/api/1.0/tagcloud.json"
             headers = {'Content-type': 'application/x-www-form-urlencoded'}
-            #args = {'body':html_text, 'strip': True}
-            #args['max_size'] = '35'
-            #args['min_size'] = '15'
-            data = urllib.urlencode(args)	
+            data = urllib.urlencode(args)
             http = httplib2.Http()
             print 'about to make api call to %s' % url
             response, content = http.request(url, 'POST', headers=headers, body=data)
@@ -43,7 +45,7 @@ def tagcloud(request):
             # note that in the template, body and style need to be given the 'safe'
             # filter so that the markup will be interpreted. otherwise it will be
             # escaped and displayed as strings.
-            return render_to_response('frontend/tagcloud_display.html', {'body' : body,
+            return render_to_template(request, 'frontend/tagcloud_display.html', {'body' : body,
                                         'style' : style})
 
 
