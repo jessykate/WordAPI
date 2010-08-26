a41a4dd2983f6cba6998f7a5acc7ab3aa5f93ae0
jessykate <jessy.cowansharp@gmail.com>
2010-08-26 13:45:20 -0400

    url setup

--- wordfrontend/frontend/urls.py
+++ wordfrontend/frontend/urls.py
@@ -1,10 +1,17 @@
 from django.conf.urls.defaults import patterns, url
+from django.views.generic.simple import direct_to_template
 from wordfrontend.frontend.views import *
 
 urlpatterns = patterns('',
   url(r'^tagcloud$', tagcloud),
   url(r'^cloud/(?P<cloud_id>\w+)', cloud),
   url(r'^save$', save),
+  url(r'^learn', direct_to_template, {'template': 'frontend/learn.html'}),
+  url(r'^create', direct_to_template, {'template': 'frontend/create.html'}),
+  url(r'^examples', direct_to_template, {'template': 'frontend/examples.html'}),
+  url(r'^browse', direct_to_template, {'template': 'frontend/browse.html'}),
+  url(r'^api', direct_to_template, {'template': 'frontend/api.html'}),
+#  url(r'^login'),
   url(r'^$', Index),
 
 )
