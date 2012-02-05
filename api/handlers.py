0b7a0619351430640445e9aa7bec400e9c635aec
Jessy Kate Schingler <jessy@jessykate.com>
2012-02-04 22:06:28 -0800

    changes for autoresize

--- api/handlers.py
+++ api/handlers.py
@@ -278,6 +278,32 @@ def fit_to_area_old(width, height, dist, font):
     font_size_fn = lambda freq: freq+b
     return font_size_fn
 
+def extrapolate_linear(dist, min_size, slope):
+	# explicitly set the indices where the min and max values can be found in
+	# the dist list. 
+	MAX = 0
+	MIN = -1
+	# y = mx+b. 
+	# m is given by the slope argument
+	# min_size occurs when x = min_freq, ie min_size = slope*min_freq + b. 
+	# rearrange for b: b = min_size - slope*min_freq
+	min_freq = float(dist[MIN][1])
+	max_freq = float(dist[MAX][1])
+	min_size = float(min_size)
+
+	# if they're all the same frequency, everything is the same size. use the
+	# mid-point between max_size and min_size (it's still built as a function 
+	# so we can use it easily in place of a dynamic value). 
+	if max_freq == min_freq:
+		font_size_fn = lambda freq: (max_size + min_size)/2.0
+	else:
+		m = slope
+		b = min_size - m*min_freq
+		font_size_fn = lambda freq: m*freq+b
+
+	print 'for the set font size, m was calculated to be %f and b calculated to be %f' % (m, b)
+	return font_size_fn
+
 def min_max_extrapolate(dist, max_size, min_size):
     # explicitly set the indices where the min and max values can be found in
     # the dist list. 
@@ -304,9 +330,8 @@ def min_max_extrapolate(dist, max_size, min_size):
     return font_size_fn
 
 def tag_cloud(dist, id_ = "", class_ = "", width=None, height=None, 
-		max_size=None, min_size=None, max_words = None, 
-		start_color=None, end_color=None, color_steps=None, 
-		sort_order="random"):
+		slope=0.15, max_words = None, start_color=None, end_color=None, 
+		color_steps=None, sort_order="random", equn="linear"):
 	''' returns a dict with style and body elements. style contains
 	defalt styling for the tag cloud, while body contains the html
 	markup. '''
@@ -323,18 +348,29 @@ def tag_cloud(dist, id_ = "", class_ = "", width=None, height=None,
 	# tagcloud) to pass back in the metadata 
 	words_in_cloud = len(dist)
 
-	# get the equation of the line for a baseline max and min size. actual
-	# fonts will be resized by javascript. 
-	max_size = 70;
-	min_size = 10;
-	font_size_fn = min_max_extrapolate(dist, max_size, min_size)
-	if not (width and height):
-		print "setting default width and height" 
-		width = 600
-		height = 400
+	# the equation of the line used here determines the *relative* sizes of
+	# different frequency words. actual font sizes are computed with javascript
+	# to dynamically fit the words into the specified area. 
 
-	# get the equation of the line between min_size and max_size. do this AFTER
-	# truncating to max_words and BEFORE shuffling the order around.  
+	if equn == "log":
+		# shifting the logarithm up by 1 gives a word of frequency one size one. 
+		font_size_fn = lambda freq: 10*math.log(freq,2) + 1
+	elif equn == "exp":  
+		font_size_fn = lambda freq: math.pow(freq, 1.2)
+	else: # linear
+		min_size = 1
+		font_size_fn = extrapolate_linear(dist, min_size, slope)
+		#font_size_fn = min_max_extrapolate(dist, max_size, min_size)
+
+	if not width:
+		print "setting default width" 
+		width = 800
+	if not height:
+		print "setting default height"
+		height = 600
+
+	# get the equation of the line AFTER truncating to max_words and BEFORE
+	# shuffling the order around.  
 
 	# determine the sort order. if the sort order is frequency, there's nothing
 	# to do since the distribution object is already sorted by frequency. 
@@ -419,13 +455,19 @@ def tag_cloud(dist, id_ = "", class_ = "", width=None, height=None,
 			'total_tags' : words_in_cloud,        
 			'short_url' : short_url,
 			}
-	record =  {'_id': oid, 
+	record =  {
+			'_id': oid, 
 			'body': body, 
 			'style': style, 
-			# not all tagclouds will necessarily have width and height?
-			'width': width,
-			'height': height,
-			'metadata': metadata} 
+			# hidden div to pass target width and height information onto javascript resizer
+			# TODO not all tagclouds will necessarily have width and height?
+			'wordcloud_size': '''
+<div id="tagcloud_size" style="width:%dpx; height:%dpx; position: absolute; left:-999em;top:-999em;"></div>''' % (width, height),
+			'script': '''<script type="text/javascript" src="http://code.jquery.com/jquery-1.7.1.min.js"></script>
+			<script type="text/javascript" src="/media/js/wordcloud.js"></script>
+			''',
+			'metadata': metadata
+			} 
 
 	print style
 
@@ -491,75 +533,77 @@ class CloudRetreiveHandler(BaseHandler):
 		return record
 
 class TagCloudBaseHandler(GeneralHandler):
-    ''' assumes body will be a blob of text, not a url. still has option to
+	''' assumes body will be a blob of text, not a url. still has option to
 	strip existing html. ''' 
-    allowed_methods = ('GET', 'POST')
-    #args_required = ['body']
-    args_optional = ['tokenizer', 'strip', 'max_words', 'normalize', 
-                      'remove_stopwords', 'sort_order',
-                     # custom color scheme options
-                      'start_color', 'end_color', 'color_steps',
-                     # width and height of the div returned
-                     'width', 'height', 
-                     # scaling factors for largest and smallest words
-                     'max_size', 'min_size']
+	allowed_methods = ('GET', 'POST')
+	#args_required = ['body']
+	args_optional = ['tokenizer', 'strip', 'max_words', 'normalize', 
+		'remove_stopwords', 'sort_order',
+		# custom color scheme options
+		'start_color', 'end_color', 'color_steps',
+		# width and height of the div returned
+		'width', 'height', 
+		# scaling factor for linear equation
+		'slope',
+		# form of the equation from which to extrapolate relative word sizes
+		'equn']
 
-    def execute(self):
-        pass
+	def execute(self):
+		pass
 
-    def escape_text(self, raw_text, encoding=None):
-        if not encoding:
-            encoding = 'utf8'
-        #ustring = unicode(raw_text, encoding, 'ignore')
-        ustring = html_unescape(raw_text)
-        return ustring
+	def escape_text(self, raw_text, encoding=None):
+		if not encoding:
+			encoding = 'utf8'
+		#ustring = unicode(raw_text, encoding, 'ignore')
+		ustring = html_unescape(raw_text)
+		return ustring
 
-    def get_text(self):
-        # return the actual contents to be used in the tag cloud. implemented
-        # by child class, depending on the call type-- eg. in-line or url
-        # reference. 
-        pass
+	def get_text(self):
+		# return the actual contents to be used in the tag cloud. implemented
+		# by child class, depending on the call type-- eg. in-line or url
+		# reference. 
+		pass
 
-    def get_tokens(self):
-        tokenizer_opts = {}
-        if 'tokenizer' in self.kwargs.keys():
-            tokenizer_opts['tokenizer'] = self.kwargs['tokenizer']
-        if 'strip' in self.kwargs.keys():
-            tokenizer_opts['strip'] = self.kwargs['strip']        
-        if 'remove_stopwords' in self.kwargs.keys():
-            tokenizer_opts['remove_stopwords'] = self.kwargs['remove_stopwords']        
-        if 'normalize' in self.kwargs.keys():
-            tokenizer_opts['normalize'] = self.kwargs['normalize']        
-        tokens =  tokenize(self.get_text(), **tokenizer_opts )
-        return tokens
+	def get_tokens(self):
+		tokenizer_opts = {}
+		if 'tokenizer' in self.kwargs.keys():
+			tokenizer_opts['tokenizer'] = self.kwargs['tokenizer']
+		if 'strip' in self.kwargs.keys():
+			tokenizer_opts['strip'] = self.kwargs['strip']        
+		if 'remove_stopwords' in self.kwargs.keys():
+			tokenizer_opts['remove_stopwords'] = self.kwargs['remove_stopwords']        
+		if 'normalize' in self.kwargs.keys():
+			tokenizer_opts['normalize'] = self.kwargs['normalize']        
+		tokens =  tokenize(self.get_text(), **tokenizer_opts )
+		return tokens
 
-    def get_freqdist(self, tokens):
-        freq = nltk.FreqDist(tokens)
-        return freq
+	def get_freqdist(self, tokens):
+		freq = nltk.FreqDist(tokens)
+		return freq
 
-    def get_cloud(self, freq):
-        cloud_opts = {}
-        if 'width' in self.kwargs.keys():
-            cloud_opts['width'] = self.kwargs['width']                
-        if 'height' in self.kwargs.keys():
-            cloud_opts['height'] = self.kwargs['height']                
-        if 'max_size' in self.kwargs.keys():
-            cloud_opts['max_size'] = self.kwargs['max_size']                
-        if 'min_size' in self.kwargs.keys():
-            cloud_opts['min_size'] = self.kwargs['min_size']                
-        if 'max_words' in self.kwargs.keys():
-            cloud_opts['max_words'] = self.kwargs['max_words']                
-        if 'sort_order' in self.kwargs.keys():
-            cloud_opts['sort_order'] = self.kwargs['sort_order']                
-            print cloud_opts['sort_order']
-        if 'start_color' in self.kwargs.keys():
-            cloud_opts['start_color'] = self.kwargs['start_color']                
-        if 'end_color' in self.kwargs.keys():
-            cloud_opts['end_color'] = self.kwargs['end_color']                
-        if 'color_steps' in self.kwargs.keys():
-            cloud_opts['color_steps'] = self.kwargs['color_steps']                
-        cloud = tag_cloud(freq, **cloud_opts)
-        return cloud
+	def get_cloud(self, freq):
+		cloud_opts = {}
+		if 'width' in self.kwargs.keys():
+			cloud_opts['width'] = self.kwargs['width']                
+		if 'height' in self.kwargs.keys():
+			cloud_opts['height'] = self.kwargs['height']                
+		if 'slope' in self.kwargs.keys():
+			cloud_opts['slope'] = self.kwargs['slope']                
+		if 'max_words' in self.kwargs.keys():
+			cloud_opts['max_words'] = self.kwargs['max_words']                
+		if 'sort_order' in self.kwargs.keys():
+			cloud_opts['sort_order'] = self.kwargs['sort_order']                
+			print cloud_opts['sort_order']
+		if 'start_color' in self.kwargs.keys():
+			cloud_opts['start_color'] = self.kwargs['start_color']                
+		if 'end_color' in self.kwargs.keys():
+			cloud_opts['end_color'] = self.kwargs['end_color']                
+		if 'color_steps' in self.kwargs.keys():
+			cloud_opts['color_steps'] = self.kwargs['color_steps']                
+		if 'equn' in self.kwargs.keys():
+			cloud_opts['equn'] = self.kwargs['equn']
+		cloud = tag_cloud(freq, **cloud_opts)
+		return cloud
 
 class TagCloudBodyHandler(TagCloudBaseHandler):
     # 'body' becomes fargs[0] in the parent class's execute() method
